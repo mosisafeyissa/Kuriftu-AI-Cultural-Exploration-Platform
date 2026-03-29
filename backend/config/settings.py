d3c5efde0c11400208c80c1cc0885d320a94dabe
annabdiyu <annaply156@gmail.com>
2026-03-29 16:32:35 -0700

    update the setting file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "artifacts",
+    "users",
+    "orders",
+    "ai_services",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
